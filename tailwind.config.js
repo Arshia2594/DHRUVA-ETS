9013266f6bff8275b36b62806ee6520e9de17502
arshiya <ansariarshiya366@gmail.com>
2025-10-14 15:07:19 +0530

    ADDED:adde Style

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,16 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  ],safelist: [
+  'bg-green-600',
+  'hover:bg-green-700',
+  'bg-blue-600',
+  'hover:bg-blue-700',
+],
+
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
